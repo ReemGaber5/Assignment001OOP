746a339c010685ab904fa906050580e8126d1247
ReemGaber <Reemgaber202001@yahoo.com>
2024-12-23 20:15:03 +0200

    Q1

--- Assignment001/Program.cs
+++ Assignment001/Program.cs
@@ -1,10 +1,32 @@
 ﻿namespace Assignment001
 {
+
+    #region Question01
+    enum WeekDays
+    {
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday,
+        Sunday
+    }
+    #endregion
+
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Question01
+            Console.WriteLine("Days of the week : ");
+            foreach (WeekDays day in Enum.GetValues(typeof(WeekDays)))
+            {
+                Console.WriteLine(day);
+            }
+
+            #endregion
+
         }
     }
 }
